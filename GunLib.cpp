f7d5534147c6fb0a1479564b458cbc2c454d461f
Dylan John <dylansejohn@gmail.com>
2023-08-30 11:24:42 +0100

    UNRELATED
    Just made an addition script for funzies yk

--- GunLib.cpp
+++ GunLib.cpp
@@ -2,7 +2,15 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int x, y;
+    int sum;
+
+    std::cout << "Type a number: ";
+    std::cin >> x;
+    std::cout << "Type another number: ";
+    std::cin >> y;
+    sum = x + y;
+    std::cout << "The sum of those numbers is: " << sum;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
