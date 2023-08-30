9908949fc534dfe974457931e8737f5b39b9e33c
Dylan John <dylansejohn@gmail.com>
2023-08-30 11:47:19 +0100

    First Gun
    It currently only gives info about the m1911, I will slowly add more guns, but I am currently looking to add a search engine type thing to sort through the guns.

--- GunLib.cpp
+++ GunLib.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
+using namespace std;
+
+class Pistols
+{
+public:
+    string Name; //name of the gun
+    string Ammo; //calibur of ammo
+    string About; //info about the gun
+    string Action; //auto, semi-auto or non-auto
+};
 
 int main()
 {
-    int x, y;
-    int sum;
+    Pistols m1911;
+    m1911.Name = "m1911\n\n";
+    m1911.Ammo = ".45 ACP\n\n";
+    m1911.About = "The M1911 (Colt 1911 or Colt Government) is a\n single-action, recoil-operated, semi-automatic\n pistol chambered for the .45 ACP cartridge.\n Designed by John Browning, the M1911\n is the best-known of his designs to use the\n short recoil principle in its basic design.\n\n";
+    m1911.Action = "Semi-Automatic\n\n";
 
-    std::cout << "Type a number: ";
-    std::cin >> x;
-    std::cout << "Type another number: ";
-    std::cin >> y;
-    sum = x + y;
-    std::cout << "The sum of those numbers is: " << sum;
+    cout << "The " << m1911.Name << ":";
+    cout << "Ammo Callibur: " << m1911.Ammo;
+    cout << "'" << m1911.About << "'";
+    cout << "Action: " << m1911.Action;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
